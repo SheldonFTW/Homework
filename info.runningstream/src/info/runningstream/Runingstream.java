cf262bd313d9ddc01486f11004e393a1de0832ed
boria <boriabritva@gmail.com>
2017-09-27 00:11:48 +0300

    Первый код

--- info.runningstream/src/info/runningstream/Runingstream.java
+++ info.runningstream/src/info/runningstream/Runingstream.java
@@ -3,7 +3,14 @@ package info.runningstream;
 public class Runingstream {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String U = "USD";
+		String E = "EUR";
+		String R = "RUB";
+		int u = 25;
+		int e = 30;
+		double r = 0.45;
+		System.out.print("Курс валют на сегодня: "+U+" "+u+", "+E+" "+e+", "+R+" "+r);		
+				
 
 	}
 
